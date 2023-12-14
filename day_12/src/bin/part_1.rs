c4b71643db19d9e4d0628328ffb01d5278093883
s-jad <s_jad@web.de>
2023-12-14 12:11:33 +0100

    Feat: solved part 1 day 12

---
+++ day_12/src/bin/part_1.rs
@@ -0,0 +1,93 @@
+use itertools::Itertools;
+use pathfinding::prelude::astar;
+
+fn heuristic(pos: (usize, usize), goal: &(usize, usize)) -> usize {
+    let (x, y) = pos;
+    let (goal_x, goal_y) = goal;
+
+    ((*goal_x as i32 - x as i32).abs() + (*goal_y as i32 - y as i32).abs()) as usize
+}
+
+fn successors(pos: (usize, usize), grid: &Vec<Vec<u8>>) -> Vec<((usize, usize), usize)> {
+    let (x, y) = pos;
+    let mut successors = Vec::new();
+
+    let offsets: [(i32, i32); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+    for (dx, dy) in offsets.iter() {
+        let new_x = (x as i32 + dx) as i32;
+        let new_y = (y as i32 + dy) as i32;
+
+        if new_x >= 0
+            && (new_x as usize) < grid[0].len()
+            && new_y >= 0
+            && (new_y as usize) < grid.len()
+        {
+            if grid[y][x] as i32 - grid[new_y as usize][new_x as usize] as i32 >= -1 {
+                successors.push(((new_x as usize, new_y as usize), 1));
+            }
+        }
+    }
+    successors
+}
+
+fn process(input: &str) -> usize {
+    let mut start: (usize, usize) = (0, 0);
+    let mut goal: (usize, usize) = (0, 0);
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            match c {
+                'S' => start = (x, y),
+                'E' => goal = (x, y),
+                _ => {}
+            }
+        }
+    }
+
+    let grid = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| {
+                    if c == 'E' {
+                        26
+                    } else if c == 'S' {
+                        1
+                    } else {
+                        c as u8 - 96
+                    }
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    astar(
+        &start,
+        |&p| successors(p, &grid),
+        |&p| heuristic(p, &goal),
+        |&p| p == goal,
+    )
+    .unwrap()
+    .1
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    let output = process(input);
+
+    dbg!(output);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
