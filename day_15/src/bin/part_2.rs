b36c26d82f9387c3cdfb8e19ce5a70f2bd92e3b6
s-jad <s_jad@web.de>
2023-12-15 16:04:13 +0100

    Feat: an incredibly slow solution t=55s

--- day_15/src/bin/part_2.rs
+++ day_15/src/bin/part_2.rs
@@ -1,41 +1,68 @@
+use geo::{line_intersection::line_intersection, Line, LineIntersection};
 use itertools::Itertools;
-use std::{collections::HashSet, time::Instant};
+use std::collections::HashMap;
+use std::time::Instant;
 
-fn find_coverage(
-    sensor: (isize, isize),
-    beacon: (isize, isize),
-    grid_min: (isize, isize),
-    grid_max: (isize, isize),
-) -> HashSet<(isize, isize)> {
-    let mut coverage = HashSet::new();
-
-    let sb_distance = (sensor.0 - beacon.0).abs() + (sensor.1 - beacon.1).abs();
-
-    let mut up = (sensor.0, sensor.1 + sb_distance);
-    let mut down = (sensor.0, sensor.1 - sb_distance);
-    let mut left = (sensor.0 - sb_distance, sensor.1);
-    let mut right = (sensor.0 + sb_distance, sensor.1);
-
-    up = (up.0.min(grid_max.0), up.1.min(grid_max.1));
-    down = (down.0.max(grid_min.0), down.1.max(grid_min.1));
-    left = (left.0.max(grid_min.0), left.1.max(grid_min.1));
-    right = (right.0.min(grid_max.0), right.1.min(grid_max.1));
-
-    for i in left.0..=right.0 {
-        for j in down.1..=up.1 {
-            if (i - sensor.0).abs() + (j - sensor.1).abs() <= sb_distance {
-                coverage.insert((i, j));
-            }
-        }
+fn most_frequent<T: std::hash::Hash + Eq>(vec: Vec<T>) -> Option<T> {
+    let mut counts = HashMap::new();
+    for item in vec {
+        *counts.entry(item).or_insert(0) += 1;
     }
 
-    coverage
+    counts
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(item, _)| item)
+}
+
+fn find_boundary(sensor: (isize, isize), beacon: (isize, isize)) -> Vec<Line<f64>> {
+    let sb_distance = (sensor.0 - beacon.0).abs() + (sensor.1 - beacon.1).abs();
+
+    let left = (sensor.0 - sb_distance) - 1;
+    let right = sensor.0 + sb_distance + 1;
+    let up = (sensor.1 - sb_distance) - 1;
+    let down = sensor.1 + sb_distance + 1;
+
+    let tl = Line::new((sensor.0 as f64, up as f64), (left as f64, sensor.1 as f64));
+    let tr = Line::new(
+        (sensor.0 as f64, up as f64),
+        (right as f64, sensor.1 as f64),
+    );
+    let bl = Line::new(
+        (sensor.0 as f64, down as f64),
+        (left as f64, sensor.1 as f64),
+    );
+    let br = Line::new(
+        (sensor.0 as f64, down as f64),
+        (right as f64, sensor.1 as f64),
+    );
+
+    vec![tl, tr, bl, br]
+}
+
+fn diagonal_walk(start: (isize, isize), end: (isize, isize)) -> Vec<(isize, isize)> {
+    let mut points = Vec::new();
+    let mut x1 = start.0;
+    let mut y1 = start.1;
+    let x2 = end.0;
+    let y2 = end.1;
+
+    let sx = if x1 - x2 > 0 { -1 } else { 1 };
+    let sy = if y1 - y2 > 0 { -1 } else { 1 };
+
+    while x1 != x2 || y1 != y2 {
+        if x1 >= 0 && x1 <= 4_000_000 && y1 >= 0 && y1 <= 4_000_000 {
+            points.push((x1, y1));
+        }
+        x1 += sx;
+        y1 += sy;
+    }
+
+    points.push((x1, y1));
+    points
 }
 
 fn process(input: &str) -> isize {
-    const GRID_MIN: (isize, isize) = (0, 0);
-    const GRID_MAX: (isize, isize) = (4_000_000, 4_000_000);
-
     let sb = input
         .lines()
         .flat_map(|l| {
@@ -48,21 +75,45 @@ fn process(input: &str) -> isize {
         })
         .collect_vec();
 
-    let mut total_coverage = HashSet::new();
-    for i in 0..sb.len() {
-        let coverage = find_coverage(sb[i].0, sb[i].1, GRID_MIN, GRID_MAX);
-        total_coverage.extend(coverage);
+    let mut bounding_lines = Vec::new();
+    for (s, b) in sb.into_iter() {
+        bounding_lines.extend(find_boundary(s, b));
     }
-    let mut signal_strength = 0;
-    for i in 0..4_000_000 {
-        for j in 0..4_000_000 {
-            if !total_coverage.contains(&(i, j)) {
-                signal_strength = (i * 4_000_000) + j;
+
+    let mut intersection_lines = Vec::new();
+
+    for i in 0..bounding_lines.len() {
+        for j in i..bounding_lines.len() {
+            match line_intersection(bounding_lines[i], bounding_lines[j]) {
+                Some(intersection) => match intersection {
+                    LineIntersection::SinglePoint {
+                        intersection: _point,
+                        ..
+                    } => {}
+                    LineIntersection::Collinear {
+                        intersection: line, ..
+                    } => {
+                        intersection_lines.push(line);
+                    }
+                },
+                None => {}
             }
         }
     }
 
-    signal_strength
+    let mut points_vec = Vec::new();
+
+    for line in intersection_lines {
+        let (start, end) = line.points();
+        points_vec.extend(diagonal_walk(
+            (start.x() as isize, start.y() as isize),
+            (end.x() as isize, end.y() as isize),
+        ));
+    }
+
+    let unobserved = most_frequent(points_vec.clone()).unwrap();
+
+    (unobserved.0 * 4_000_000) + unobserved.1
 }
 
 fn main() {
